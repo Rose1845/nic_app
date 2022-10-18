03edd772bfeae362fbd413ae8bf42c38790f90dc
Rose Odhiambo <odhiamborose466@gmail.com>
2022-10-18 17:40:28 +0300

    added user icon

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,17 +1,23 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import { FaUser } from 'react-icons/fa'
 
 const Header = () => {
   return (
-    <div className='bg-gray-400 '>
+    <div className='flex bg-gray-400 '>
       <nav className='max-w-7xl w-full flex justify-between items-center p-5'>
         <div>
         <h1 className='font-bold text-2xl'>NIC.</h1>
         </div>
-      <Link to={'/'}className="font-bold text-white">Home</Link>
+       <div className='md:none flex justify-between items-center'>
+       <Link to={'/'}className="font-bold text-white">Home</Link>
        <Link to={'/about'}className="font-bold text-white hover:text-gray-600">About</Link>
        <Link to={'/services'}className="font-bold text-white hover:text-gray-600">Services</Link>
        <Link to={'/contact'}className="font-bold text-white hover:text-gray-600">Contact</Link>
+       </div>
+       <div>
+       <FaUser/>
+       </div>
       </nav>
       
     </div>
--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -9,7 +9,7 @@ const Contact = () => {
           <input className='border-solid outline-2 outline-blue-500/50  dark:bg-slate-800 p-3 mb-4'placeholder='email address' type="email" name="" id="" />
           <input className='outline-2 outline-blue-500/50  p-3 dark:bg-slate-600 mb-4'type="text" name="" id=""placeholder='name' />
           <textarea className='outline-2 outline-blue-500/50 mb-4' placeholder='message here 'name="" id="" cols="30" rows="10"></textarea>
-          <button className='bg-sky-500 rounded-full text-black p-3 m-1' onclick={(e)=>{
+          <button className='bg-sky-500 rounded-full text-black p-3 m-1' onClick={(e)=>{
             e.preventDefault()
           }
           }>SUBMIT</button>
