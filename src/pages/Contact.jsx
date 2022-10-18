7b7f156cbe0c4c6beea0d7df01ad35e003346036
Rose Odhiambo <odhiamborose466@gmail.com>
2022-10-18 16:06:56 +0300

    added the services details

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -2,7 +2,19 @@ import React from 'react'
 
 const Contact = () => {
   return (
-    <div className='container'>
+    <div className='container mx-auto shadow-md '>
+      <h1 className='text-center text-2xl '>Contact Us</h1>
+      <form action="">
+        <div className='flex flex-col justify-center items-center'>
+          <input className='border-solid outline-2 outline-blue-500/50  dark:bg-slate-800 p-3 mb-4'placeholder='email address' type="email" name="" id="" />
+          <input className='outline-2 outline-blue-500/50  p-3 dark:bg-slate-600 mb-4'type="text" name="" id=""placeholder='name' />
+          <textarea className='outline-2 outline-blue-500/50 mb-4' placeholder='message here 'name="" id="" cols="30" rows="10"></textarea>
+          <button className='bg-sky-500 rounded-full text-black p-3 m-1' onclick={(e)=>{
+            e.preventDefault()
+          }
+          }>SUBMIT</button>
+        </div>
+      </form>
     
     </div>
   )
--- src/pages/Services.jsx
+++ src/pages/Services.jsx
@@ -4,8 +4,70 @@ import people from '../data'
 export default function Services() {
     return (
       <div flex flex-col>
+        
         <h1 className='text-3xl pt-5 font-bold text-center'>Our Services</h1>
-        <div className=''>
+        <div className='flex'>
+        <div>
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path d="M176 24c0-13.3-10.7-24-24-24s-24 10.7-24 24V64c-35.3 0-64 28.7-64 64H24c-13.3 0-24 10.7-24 24s10.7 24 24 24H64v56H24c-13.3 0-24 10.7-24 24s10.7 24 24 24H64v56H24c-13.3 0-24 10.7-24 24s10.7 24 24 24H64c0 35.3 28.7 64 64 64v40c0 13.3 10.7 24 24 24s24-10.7 24-24V448h56v40c0 13.3 10.7 24 24 24s24-10.7 24-24V448h56v40c0 13.3 10.7 24 24 24s24-10.7 24-24V448c35.3 0 64-28.7 64-64h40c13.3 0 24-10.7 24-24s-10.7-24-24-24H448V280h40c13.3 0 24-10.7 24-24s-10.7-24-24-24H448V176h40c13.3 0 24-10.7 24-24s-10.7-24-24-24H448c0-35.3-28.7-64-64-64V24c0-13.3-10.7-24-24-24s-24 10.7-24 24V64H280V24c0-13.3-10.7-24-24-24s-24 10.7-24 24V64H176V24zM160 128H352c17.7 0 32 14.3 32 32V352c0 17.7-14.3 32-32 32H160c-17.7 0-32-14.3-32-32V160c0-17.7 14.3-32 32-32zm192 32H160V352H352V160z"/></svg>
+        <figure class="bg-slate-100 rounded-xl p-8 dark:bg-slate-800">
+        <FontAwesomeIcon icon="fa-sharp fa-solid fa-network-wired" />
+          <div class="pt-6 t space-y-4">
+          <blockquote>
+           <p class="text-center text-lg font-medium">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam unde vitae accusamus aut ut, voluptatibus, impedit, adipisci sed libero esse sint saepe doloremque nostrum odit debitis quos expedita quo possimus?
+           </p>
+           </blockquote>
+         <figcaption class="font-medium">
+            <div class="text-sky-500 dark:text-sky-400">
+              Networking
+             </div>
+              <div class="text-slate-700 dark:text-slate-500">
+              Nyayo Stadium , Nairobi
+             </div>
+          </figcaption>
+         </div>
+        </figure>
+        </div>
+        <div>
+        <figure class="bg-slate-100 rounded-xl p-8 dark:bg-slate-800">
+        <FontAwesomeIcon icon="fa-solid fa-code-merge" />
+          <div class="pt-6 t space-y-4">
+          <blockquote>
+           <p class="text-center text-lg font-medium">
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Obcaecati aperiam nisi vel a alias soluta dolor, accusamus architecto nihil sunt ea minima numquam voluptas? Optio maiores corporis excepturi neque veritatis?
+           </p>
+           </blockquote>
+         <figcaption class="font-medium">
+            <div class="text-sky-500 dark:text-sky-400">
+             CyberSecurity
+             </div>
+              <div class="text-slate-700 dark:text-slate-500">
+              Nyayo Stadium, Nairobi
+             </div>
+          </figcaption>
+         </div>
+        </figure>
+        </div>
+        <div>
+        <figure class="bg-slate-100 rounded-xl p-8 dark:bg-slate-800">
+        <FontAwesomeIcon icon="fa-sharp fa-solid fa-microchip" />
+          <div class="pt-6 t space-y-4">
+          <blockquote>
+           <p class="text-center text-lg font-medium">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus, pariatur optio, quisquam blanditiis reprehenderit libero assumenda consequuntur, aspernatur nisi dicta deleniti ipsam? Laborum fuga, atque consequatur incidunt libero fugit dolore.
+           </p>
+           </blockquote>
+         <figcaption class="font-medium">
+            <div class="text-sky-500 dark:text-sky-400">
+              Software Engineering
+             </div>
+              <div class="text-slate-700 dark:text-slate-500">
+              Nyayo Stadium, Nairobi
+             </div>
+          </figcaption>
+         </div>
+        </figure>
+        </div>
           
           
         </div>
@@ -27,6 +89,7 @@ export default function Services() {
           </li>
         ))}
       </ul>
+     
         </div>
       
       </div>
