3e0bbab7279524c283ce61060f456f68eea89177
Rose Odhiambo <odhiamborose466@gmail.com>
2022-10-18 16:07:59 +0300

    removed the svg in services

--- src/pages/Services.jsx
+++ src/pages/Services.jsx
@@ -8,9 +8,9 @@ export default function Services() {
         <h1 className='text-3xl pt-5 font-bold text-center'>Our Services</h1>
         <div className='flex'>
         <div>
-        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path d="M176 24c0-13.3-10.7-24-24-24s-24 10.7-24 24V64c-35.3 0-64 28.7-64 64H24c-13.3 0-24 10.7-24 24s10.7 24 24 24H64v56H24c-13.3 0-24 10.7-24 24s10.7 24 24 24H64v56H24c-13.3 0-24 10.7-24 24s10.7 24 24 24H64c0 35.3 28.7 64 64 64v40c0 13.3 10.7 24 24 24s24-10.7 24-24V448h56v40c0 13.3 10.7 24 24 24s24-10.7 24-24V448h56v40c0 13.3 10.7 24 24 24s24-10.7 24-24V448c35.3 0 64-28.7 64-64h40c13.3 0 24-10.7 24-24s-10.7-24-24-24H448V280h40c13.3 0 24-10.7 24-24s-10.7-24-24-24H448V176h40c13.3 0 24-10.7 24-24s-10.7-24-24-24H448c0-35.3-28.7-64-64-64V24c0-13.3-10.7-24-24-24s-24 10.7-24 24V64H280V24c0-13.3-10.7-24-24-24s-24 10.7-24 24V64H176V24zM160 128H352c17.7 0 32 14.3 32 32V352c0 17.7-14.3 32-32 32H160c-17.7 0-32-14.3-32-32V160c0-17.7 14.3-32 32-32zm192 32H160V352H352V160z"/></svg>
+       
         <figure class="bg-slate-100 rounded-xl p-8 dark:bg-slate-800">
-        <FontAwesomeIcon icon="fa-sharp fa-solid fa-network-wired" />
+        {/* <FontAwesomeIcon icon="fa-sharp fa-solid fa-network-wired" /> */}
           <div class="pt-6 t space-y-4">
           <blockquote>
            <p class="text-center text-lg font-medium">
@@ -30,7 +30,7 @@ export default function Services() {
         </div>
         <div>
         <figure class="bg-slate-100 rounded-xl p-8 dark:bg-slate-800">
-        <FontAwesomeIcon icon="fa-solid fa-code-merge" />
+        {/* <FontAwesomeIcon icon="fa-solid fa-code-merge" /> */}
           <div class="pt-6 t space-y-4">
           <blockquote>
            <p class="text-center text-lg font-medium">
@@ -50,7 +50,7 @@ export default function Services() {
         </div>
         <div>
         <figure class="bg-slate-100 rounded-xl p-8 dark:bg-slate-800">
-        <FontAwesomeIcon icon="fa-sharp fa-solid fa-microchip" />
+        {/* <FontAwesomeIcon icon="fa-sharp fa-solid fa-microchip" /> */}
           <div class="pt-6 t space-y-4">
           <blockquote>
            <p class="text-center text-lg font-medium">
